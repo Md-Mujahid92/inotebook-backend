69569f8c6ea3d3d996c966a8480056fdc86bb9c4
Md-Mujahid92 <120643682+Md-Mujahid92@users.noreply.github.com>
2023-09-22 18:06:16 +0530

    comiit

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const express = require("express");
 connectToMongo();
 const app = express();
 const port = 5000;
+var cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 
+// Available Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`iNotebook backend listening at http://localhost:${port}`);
 });
